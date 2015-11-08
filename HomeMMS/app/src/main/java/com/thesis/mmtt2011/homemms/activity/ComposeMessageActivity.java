46caa5a671e326091cc5dfd828407024ea782b44
YPham <pxy.uit@gmail.com>
2015-11-08 12:08:06 +0700

    Option menu for compose message activity.

--- HomeMMS/app/src/main/java/com/thesis/mmtt2011/homemms/activity/ComposeMessageActivity.java
+++ HomeMMS/app/src/main/java/com/thesis/mmtt2011/homemms/activity/ComposeMessageActivity.java
@@ -1,10 +1,13 @@
 package com.thesis.mmtt2011.homemms.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,4 +44,32 @@ public class ComposeMessageActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_compose_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_camera) {
+            /*Intent intent = new Intent(this, ScanDevicesActivity.class);
+            startActivity(intent);*/
+            return true;
+        }
+
+        if (id == R.id.action_video) {
+            //
+            return true;
+        }
+
+        if (id == R.id.action_record) {
+            //
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
