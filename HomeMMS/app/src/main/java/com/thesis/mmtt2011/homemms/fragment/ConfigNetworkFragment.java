2553a192f3761c4fc48e8c4b1ef43655c3baa2a8
Y Pham <pxy.uit@gmail.com>
2015-12-10 23:16:17 +0700

    add config network

---
+++ HomeMMS/app/src/main/java/com/thesis/mmtt2011/homemms/fragment/ConfigNetworkFragment.java
@@ -0,0 +1,32 @@
+package com.thesis.mmtt2011.homemms.fragment;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thesis.mmtt2011.homemms.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class ConfigNetworkFragment extends Fragment {
+
+
+    public ConfigNetworkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_config_network, container, false);
+    }
+
+}
